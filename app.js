d73f5a6de56cb1eeb8012d0fd9ae26e68b4322e4
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-03-04 01:44:01 +0600

    added modal

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const displayData = (posts) => {
 
     // postContainer.innerHTML = '';
     console.log(posts);
-    const ShowAll = document.getElementById('show-all');
-    if (posts.length > 6) {
-        posts = posts.slice(0, 6);
-        ShowAll.classList.remove('d-none');
-    }
-    else {
-        ShowAll.classList.add('d-none');        
-    }
+    // const ShowAll = document.getElementById('show-all');
+    // if (posts.length > 6) {
+    //     posts = posts.slice(0, 6);
+    //     ShowAll.classList.remove('d-none');
+    // }
+    // else {
+    //     ShowAll.classList.add('d-none');        
+    // }
 
 
     // loop every single post
@@ -52,7 +52,7 @@ const displayData = (posts) => {
                         <p class="card-text"><i class="fa-solid fa-calendar-days"></i> ${formattedDate}</p>
                     </div>
                     <div>
-                        <a style="background-color: #FEF7F7;" class="p-3 rounded-5 text-danger" href="#"><i class="fa-solid fa-arrow-right"></i></a>
+                        <a onclick="loadSingleData('${post.id}')" style="background-color: #FEF7F7;" class="p-3 rounded-5 text-danger" data-bs-toggle="modal" data-bs-target="#postDetailsModal" href="#"><i class="fa-solid fa-arrow-right"></i></a>
                     </div>
                 </div>
 
@@ -65,9 +65,9 @@ const displayData = (posts) => {
 
 
 // load show all data
-document.getElementById('show-all-btn').addEventListener('click', function () {
-    loadAllData();
-})
+// document.getElementById('show-all-btn').addEventListener('click', function () {
+//     loadAllData();
+// })
 
 
 // // loader or spinner
@@ -81,6 +81,71 @@ document.getElementById('show-all-btn').addEventListener('click', function () {
 //     }
 // }
 
+
+const loadSingleData = async id => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayDataDetails(data.data);
+}
+
+const displayDataDetails = post => {
+    console.log(post);
+    document.getElementById('postDetailsModalLabel').innerText = post.description;
+    const postDetails = document.getElementById('modal-body');
+    postDetails.innerHTML = `
+        <div>
+            <div class="col d-flex justify-content-between align-items-center gap-2">
+            <div class="card h-100 w-100">
+            <div class="card-body">
+            <h5 class="card-title text-success">${post.pricing[0].price}</h5>
+            <h5 class="card-title text-success">${post.pricing[0].plan}</h5>
+            </div>
+            </div>
+
+            <div class="card h-100 w-100">
+            <div class="card-body">
+            <h5 class="card-title text-warning">${post.pricing[1].price}</h5>
+            <h5 class="card-title text-warning">${post.pricing[1].plan}</h5>
+            </div>
+            </div>
+
+            <div class="card h-100 w-100">
+            <div class="card-body">
+            <h5 class="card-title text-danger">${post.pricing[2].price}</h5>
+            <h5 class="card-title text-danger">${post.pricing[2].plan}</h5>
+            </div>
+            </div>
+        </div>
+
+
+
+        <div class="pt-3 d-flex gap-2 justify-content-between">
+        <div>
+            <h4>Features</h4>
+            <ul>
+                <li>${post.features[1].feature_name}</li>
+                <li>${post.features[2].feature_name}</li>
+                <li>${post.features[3].feature_name}</li>
+            </ul>
+        </div>
+
+        <div>
+            <h4>Integrations</h4>
+            <ul>
+                <li>${post.integrations[0]}</li>
+                <li>${post.integrations[1]}</li>
+                <li>${post.integrations[2]}</li>
+            </ul>
+        </div>
+    </div>
+   </div>
+
+
+    `
+
+
+}
 
 
 loadAllData();
